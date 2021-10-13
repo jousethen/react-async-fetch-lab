98ab8c1e6b328234363423332a0a1037247086cc
jousethen <jthen93@gmail.com>
2021-10-13 14:13:02 -0400

    commit tree a27a610d4fdcdfb0c64c3b8f06405965e2ddbebe

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((data) => {
+
+      });
+  }
+
+  render() {
+    return (
+      < h1 > Hi</h1 >
+    )
+  }
+}
